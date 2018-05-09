2f31c2ce55ed49abb177e482cfc6216235f0c023
Ali <ali.mohamed8919@yahoo.com>
2018-05-09 16:52:53 -0400

    new classes
    added audio class and gameoverstate

--- src/GameState/GameOverState.java
+++ src/GameState/GameOverState.java
@@ -1,9 +1,22 @@
 package GameState;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+
+import Audio.AudioPlayer;
+import Main.Background;
 
 public class GameOverState extends GameState {
 	
+	private Background bg;
+	
+	private AudioPlayer bgMusic;
+	
+	private Font font;
+	
+	private int currentChoice = 0;
+	private String[] options = {"RETRY", "QUIT"};
 	
 	public GameOverState(GameStateManager gsm) {
 		
@@ -33,8 +46,24 @@ public class GameOverState extends GameState {
 
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
+		bg.draw(g);
 		
+
+		g.setColor(Color.RED);
+		g.setFont(new Font("Comic Sans", Font.BOLD, 30));
+		
+		
+		// draw menu options
+		g.setFont(font);
+		for(int i = 0; i < options.length; i++) {
+			if(i == currentChoice) {
+				g.setColor(Color.WHITE);
+			}
+			else {
+				g.setColor(Color.DARK_GRAY);
+			}
+			g.drawString(options[i], 350 + i * 10, 450 + i * 30);
+		}
 	}
 
 }
